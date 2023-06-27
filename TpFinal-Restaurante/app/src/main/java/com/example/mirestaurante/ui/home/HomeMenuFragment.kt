fc9384db761dac9b8811d3d26e635998a2b28cd4
Rodrigo <pasquali.rodrigo@hotmail.com>
2023-06-27 03:29:26 -0300

    Tp Restaurante(feat): se agrega navegacion desde los botones de la pantalla menu

--- TpFinal-Restaurante/app/src/main/java/com/example/mirestaurante/ui/home/HomeMenuFragment.kt
+++ TpFinal-Restaurante/app/src/main/java/com/example/mirestaurante/ui/home/HomeMenuFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.mirestaurante.R
 import com.example.mirestaurante.databinding.FragmentMenuBinding
 
 class HomeMenuFragment : Fragment() {
@@ -20,6 +22,34 @@ class HomeMenuFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         binding = FragmentMenuBinding.inflate(inflater, container, false)
+
+        onBebidasButtonClick()
+        onPlatosButtonClick()
+
         return binding.root
     }
+
+    private fun onBebidasButtonClick() {
+        binding.bebidas.setOnClickListener {
+//            changeFragment(BebidasFragment())
+            this.findNavController().navigate(R.id.action_nav_menu_home_to_nav_bebidas)
+        }
+    }
+
+    private fun onPlatosButtonClick() {
+        binding.platos.setOnClickListener {
+            this.findNavController().navigate(R.id.action_nav_menu_home_to_nav_platos)
+        }
+    }
+
+//    private fun changeFragment(fragment: Fragment) {
+////        fragment.let {
+////            parentFragmentManager.beginTransaction()
+////                .replace(R.id.nav_host_fragment_content_navigation_drawer, it)
+////                .addToBackStack("menu")
+////                .commit()
+////        }
+//
+//        this.findNavController().navigate(R.id.action_nav_menu_home_to_nav_bebidas)
+//    }
 }
