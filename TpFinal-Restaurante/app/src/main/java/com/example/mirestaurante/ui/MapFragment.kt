f7157cc5c9e6d1dab2c67b867b6f86882fa258ca
Rodrigo <pasquali.rodrigo@hotmail.com>
2023-07-04 00:31:03 -0300

    Tp Restaurante(feat): se agrega botones de zoom y marca del restaurante en el mapa

--- TpFinal-Restaurante/app/src/main/java/com/example/mirestaurante/ui/MapFragment.kt
+++ TpFinal-Restaurante/app/src/main/java/com/example/mirestaurante/ui/MapFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mirestaurante.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentMapBinding
@@ -15,7 +19,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMapBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -23,9 +27,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(binding.googleMap != null) {
-            setupMap()
-        }
+        setupMap()
     }
 
     private fun setupMap() {
@@ -34,7 +36,22 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         binding.googleMap.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(map: GoogleMap) {
+        map.mapType = GoogleMap.MAP_TYPE_TERRAIN
 
+        val myLocation = LatLng(-34.6032649,-58.3863535)
+
+        val cameraPosition = CameraPosition.Builder()
+            .target(myLocation)
+            .zoom(16f)
+            .build()
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        val locationMark = MarkerOptions().position(myLocation)
+
+        map.apply {
+            animateCamera(cameraUpdate)
+            addMarker(locationMark)
+            uiSettings.isZoomControlsEnabled = true
+        }
     }
 }
