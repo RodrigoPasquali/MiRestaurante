fa1483e7785f861004e6acf184ec1dea7b8c2d0f
Rodrigo <pasquali.rodrigo@hotmail.com>
2023-08-14 18:31:58 -0300

    MiRestaurante(feat): se crea test para SaveProducts()

---
+++ TpFinal-Restaurante/app/src/test/java/com/example/mirestaurante/domain/action/product/SaveProductsTest.kt
@@ -0,0 +1,44 @@
+package com.example.mirestaurante.domain.action.product
+
+import com.example.mirestaurante.domain.repository.ProductRepository
+import com.example.mirestaurante.infraestructure.database.PlatosListDummy
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SaveProductsTest {
+    private lateinit var saveProducts: SaveProducts
+    private val repository = mockk<ProductRepository> (relaxed = true)
+
+    @Before
+    fun setup() {
+        saveProducts = SaveProducts(repository)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `save products successfully`() = runTest() {
+//        coEvery { repository.saveProducts(PRODUCT_LIST) } returns Unit //esto es lo mismo que usar mockk(relaxed = true)
+        whenSaveProducts()
+
+        thenSaveProductsSuccessfully()
+    }
+    
+    private suspend fun whenSaveProducts() {
+        saveProducts(PRODUCT_LIST)
+    }
+
+    private fun thenSaveProductsSuccessfully() {
+        coVerify {
+            repository.saveProducts(PRODUCT_LIST)
+        }
+    }
+
+    private companion object {
+        val PRODUCT_LIST = PlatosListDummy.getPlatos()
+    }
+}
